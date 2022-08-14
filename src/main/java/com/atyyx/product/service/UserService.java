9c096f68cd9735d2124634660281d5dad3e0d24f
DESKTOP-9NUMGEK\yangyuxin <592996903@qq.com>
2022-08-14 23:40:42 +0800

    用户业务逻辑层的接口，需要继承MybatisPlus给我们提供的IService类，泛型用对应的实体类即可。

---
+++ src/main/java/com/atyyx/product/service/UserService.java
@@ -0,0 +1,12 @@
+package com.atyyx.product.service;
+
+import com.atyyx.product.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author yyx
+ * @version 1.0
+ * @date 2022/8/14 21:58
+ */
+public interface UserService extends IService<User> {
+}
